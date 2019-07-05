2423812a09728ac1fac1973cc9d18606865449d6
valeria <valeriagrandi@hotmail.com>
2019-07-05 09:50:03 -0300

    probando push

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ mongoConnection = require("./src/mongo/mongoConnection")
 Home = require("./src/mongo/mongoHome")
 mongoConnection.connect( (db) => {
   
-    gastoHome = new Home("gastos", db)
-  gastoHome.insert(new Gasto(12/01/2001, "hola"))
-  server.register(gastoHome)
+gastoHome = new Home("gastos", db)
+gastoHome.insert(new Gasto(12/01/2001, "hola"))
+server.register(gastoHome)
   
   server.init();
 })
